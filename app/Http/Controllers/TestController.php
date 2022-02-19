8064328b7f1e195e48a30b9449ccbff348587f83
Ahmed Mohamed Abd El Ftah <ahmedabdelftah95165@gmail.com>
2022-02-19 11:56:43 +0200

    Add dynamic data and bootstrap

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,12 +9,11 @@ class TestController extends Controller
     public function testAction()
     {
         $users = [
-            ['id' => 1, 'name' => 'ahmed'],
-            ['id' => 2, 'name' => 'mohamed'],
+            ['id' => 1, 'title' => 'first post', 'posted_by' => 'ahmed', 'created_at' => '2022-02-19 10:00:00'],
+            ['id' => 2, 'title' => 'second post', 'posted_by' => 'mohamed', 'created_at' => '2022-02-15 05:00:00'],
         ];
         return view('test',[
             'users' => $users,
-            'greeting' => 'Hello this is some var from web.php',
         ]);
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,7 +1,60 @@
-hello ahmed
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">ITI Blog</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">All Posts</a>
+              </li>
+  
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-@foreach ($users as $user)
-    <p>This is user {{ $user['name'] }}</p>
-@endforeach
+      <div class="container">
 
-{{$greeting}}
+        <div class="text-center mt-5">
+            <a href="#" class="btn btn-success ">Create Post</a>
+
+        </div>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Posted By</th>
+                <th scope="col">Created At</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <th scope="row">{{$user['id']}}</th>
+                    <td>{{$user['title']}}</td>
+                    <td>{{$user['posted_by']}}</td>
+                    <td>{{$user['created_at']}}</td>
+                    <td><a href="#" class="btn btn-info">View</a></td>
+                </tr>
+            @endforeach
+            </tbody>
+          </table>
+      </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
